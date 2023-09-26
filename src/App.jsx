376f6b05e68e35f1f524ce96669a9dc5fcf10a84
Amr-Shwieky2 <131922418+Amr-Shwieky2@users.noreply.github.com>
2023-09-26 04:41:33 +0300

    edgeledger_website

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,12 @@ function App() {
           <Contact/>
         </main>
 
-      <footer class="footer bg-dark">
-        <div class="social">
-          <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
-          <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
-          <a href="#"><i class="fab fa-youtube fa-2x"></i></a>
-          <a href="#"><i class="fab fa-linkedin fa-2x"></i></a>
+      <footer className="footer bg-dark">
+        <div className="social">
+          <a href="#"><i className="fab fa-facebook fa-2x"></i></a>
+          <a href="#"><i className="fab fa-twitter fa-2x"></i></a>
+          <a href="#"><i className="fab fa-youtube fa-2x"></i></a>
+          <a href="#"><i className="fab fa-linkedin fa-2x"></i></a>
         </div>
         <p>Copyright &copy; 2020 - EdgeLedger</p>
       </footer>
